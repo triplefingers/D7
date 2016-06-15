84f33357afba68f1d2674a34c8072e826f7515be
Qjin Cho <qjin86@gmail.com>
2016-06-15 20:29:40 +0900

    (feat) Add routers for ajax requests from client
    Added cases for ajax requests from client side in server/router.js.
    Modified .gitignore to include npm-debug.log.

--- server/router.js
+++ server/router.js
@@ -1,9 +1,64 @@
-var express = require("express");
+import express from 'express';
 
-var router = express.Router();
+//dummy data 가져오기
+import dummy from 'db/temp';
 
-router.get("/home", function(req, res, next) {
-  res.end();
-})
+const router = express.Router();
+
+// router.get("/home", (req, res, next) => {
+//   res.end();
+// });
+
+router.get("/api/user", (req, res, next) => {
+  let url = req.path;
+  let query = req.query;
+  console.log('QUERY', query);
+  console.log('URL', url);
+  res.send(200, dummy.user);
+});
+
+router.get("/api/projects", (req, res, next) => {
+  let url = req.path;
+  let query = req.query;
+  console.log('QUERY', query);
+  console.log('URL', url);
+  if (query.type === "all"){
+    res.send(200, dummy.all);
+  } else if (query.type === "ongoing"){
+    res.send(200, dummy.all.ongoing);
+  } else if (query.type === "recommended"){
+    res.send(200, dummy.recommended);
+  } else {
+    res.send(404, "404 - Bad request");
+  }
+});
+
+router.get("/api/project", (req, res, next) => {
+  let url = req.url;
+  let query = req.query;
+  console.log('QUERY', query);
+  console.log('URL', url);
+  res.send(200, dummy.project);
+});
+
+router.post("/api/record", (req, res, next) => {
+  let url = req.url;
+  let body = req.body;
+  console.log('BODY', body);
+  console.log('URL', url);
+  res.send(200, "data response for recording today");
+});
+
+router.post("/api/newproject", (req, res, next) => {
+  let url = req.url;
+  let body = req.body;
+  console.log('BODY', body);
+  console.log('URL', url);
+  res.send(200, "data response for creating new project");
+});
+
+router.get("*", (req, res, next) => {
+  res.send(404, '404 - uhehehe');
+});
 
 module.exports = router;
