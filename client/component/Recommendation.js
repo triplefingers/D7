f0fa5f144179cfb9600c388ba665216c8568fa74
lennyKim <heliotherapyy@gmail.com>
2016-06-18 14:38:47 +0900

    (fix) reset AppContainer's state when mounted

--- client/component/Recommendation.js
+++ client/component/Recommendation.js
@@ -3,29 +3,37 @@ import React, {Component} from "react";
 class Recommendation extends Component {
   constructor(props) {
     super(props);
+    this.state = {clicked: "none"};
   }
 
   componentDidMount() {
-    // have to edit userId
+    this.props.reset();
     this.props.fetchRecommendation("1234");
     console.log("Recommendation mounted");
   }
 
   render() {
     let list = [];
+    let popUpStyle = {display: this.state.clicked};
 
     if (this.props.data.list) {
       list = this.props.data.list;
     }
 
+
     list = list.map((project) => (
-        <li onClick={() => {this.props._save({id: project.id, title: project.title, description: project.description, onDay: project.onDay, status: "ongoing"}); this.props.goto("/history/project/" + project.id);}}>{"프로젝트" + " " + project.title}</li>
+        <li onClick={() => {this.props._save({title: project.title, description: project.description}); this.setState({clicked: "initial"});}}>{"프로젝트" + " " + project.title}</li>
       ));
 
     return (
       <div>
         <h1>Recommended</h1>
         {list}
+
+        <div style={popUpStyle}>
+          <button onClick={() => this.props.goto("/create/date")}>start this project</button>
+          <button onClick={() => this.setState({clicked: "none"})}>cancel</button>
+        </div>
       </div>
 
     );
