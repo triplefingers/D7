5e817ed230bb519949970ce813908ad86a68d71e
Qjin Cho <qjin86@gmail.com>
2016-06-18 17:24:35 +0900

    (feat) Add status(onDay) checking process to fetchAllProjects api method

--- server/db/api/projects.js
+++ server/db/api/projects.js
@@ -8,17 +8,42 @@ const fetchAllProjects = (url, q, res)=>{
   let up = model.UserProject.where("userId", 1).fetchAll({withRelated: [
     "project"
   ]}).then((projects)=>{
-    return projects.map((up)=>{
+    const result = {
+      waiting: [],
+      ongoing: [],
+      complete: []
+    };
+    const today = new Date();
+    projects.forEach((up)=>{
       console.log(">>>>>>>>>>>>>>>>>>>>>", up.id);
-      // console.log(">>>>>>>>>>>>>>>>>>>>>", up.related("project").toJSON().title);
       let project = up.related("project").toJSON();
-      return {
+      up = up.toJSON();
+      let startAt = up.startAt;
+      console.log('STARTAT', startAt);
+      console.log('UP.STARTAT', up.startAt);
+      startAt =  new Date(startAt);
+      let data = {
         id: up.id,
         title: project.title,
         description: project.description
       };
-      // return up;
+
+      /* Check Project status */
+      console.log(today.valueOf());
+      console.log(startAt.valueOf());
+      let diff = today.valueOf() - startAt.valueOf();
+      diff = Math.ceil(diff/(60*60*24*1000));
+      console.log('DIFF', diff);
+      if ( diff <= 0 ){
+        result.waiting.push(data);
+      } else if ( diff > 0 && diff <= 7 ){
+        data.onDay = diff;
+        result.ongoing.push(data);
+      } else {
+        result.complete.push(data);
+      }
     });
+    return result;
   }).then((data)=>res.status(200).send(data));
 
 };
