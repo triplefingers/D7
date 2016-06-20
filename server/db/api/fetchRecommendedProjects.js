4c7c2554682d4c79ee63d586c0672fa6aa70b5b9
Qjin Cho <qjin86@gmail.com>
2016-06-20 15:14:59 +0900

    (fix) Remove unused codes in api Methods

--- server/db/api/fetchRecommendedProjects.js
+++ server/db/api/fetchRecommendedProjects.js
@@ -2,11 +2,9 @@ import model from "../models";
 import collection from "../collections";
 
 const fetchRecommendedProjects = (url, q, res)=>{
-  const result = [];
   collection.Projects.orderBy("-wish").fetch().then((projects)=>{
     return projects.slice(0, 4);
   }).then((data)=>res.status(200).send(data));
-
 };
 
 export default fetchRecommendedProjects;
