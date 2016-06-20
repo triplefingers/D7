4f424997c4c927f7e9ef04daba426505f0a9fa59
Dongwoo Kim <idforcoding@gmail.com>
2016-06-20 22:42:02 +0900

    (fix) fix spaces

--- server/db/api/fetchRecommendedProjects.js
+++ server/db/api/fetchRecommendedProjects.js
@@ -1,7 +1,7 @@
 import model from "../models";
 import collection from "../collections";
 
-const fetchRecommendedProjects = (url, q, res)=>{
+const fetchRecommendedProjects = (url, q, res) => {
   collection.Projects.orderBy("-wish").fetch().then((projects)=>{
     return projects.slice(0, 4);
   }).then((data)=>res.status(200).send(data));
