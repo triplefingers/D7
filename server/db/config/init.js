321be5d72ba559a0d2505989c9cc7526b71b9d67
Dongwoo Kim <idforcoding@gmail.com>
2016-06-27 15:17:39 +0900

    (refactor) add more dummies according to schema refactor

--- server/db/config/init.js
+++ server/db/config/init.js
@@ -9,13 +9,21 @@ const initDB = () => {
   new model.User(
     { username: "Lenny Kim",
       email: "idforcoding@gmail.com",
-      password: "12345678"
+      password: "12345678",
+      photo: "http://blog.room34.com/wp-content/uploads/underdog/logo.thumbnail.png"
     }).save().then((model) => console.log("Created User model: ", model.attributes.username));
 
   /* Project model data 생성 */
   InitialData.ProjectData.forEach((item) => {
     new model.Project(item).save()
     .then((model) => console.log("Created Project model", model.attributes.title));
+  });
+
+  /* Wish model data 생성 */
+  InitialData.WishData.forEach((item) => {
+    new model.Wish(item).save()
+    .then((model) => console.log("Created wish model", model.attributes.userId, model.attributes.projectId))
+    .catch((err) => console.error("Error in WishData initialization", err))
   });
 
   /* userProject model data 생성 */
@@ -30,6 +38,17 @@ const initDB = () => {
     .then((model) => console.log("Created Post model", model.attributes.text));
   });
 
+  /* Like model data 생성 */
+  InitialData.LikeData.forEach((item) => {
+    new model.Like(item).save()
+    .then((model) => console.log("Created Like model", model.attributes.userId, model.attributes.postId));
+  });
+
+  /* Report model data 생성 */
+  InitialData.ReportData.forEach((item) => {
+    new model.Report(item).save()
+    .then((model) => console.log("Created Report model", model.attributes.description));
+  })
 
   /* PostImage model data 생성 */
   InitialData.PostImageData.forEach((item)=>{
@@ -37,6 +56,11 @@ const initDB = () => {
     .then((model)=>console.log("Created PostImage model", model.attributes.url));
   });
 
+  /* Transaction model data 생성 */
+  InitialData.TransactionData.forEach((item) => {
+    new model.Transaction(item).save()
+    .then((model) => console.log("Created Transaction model", model.attributes));
+  });
 };
 
 export default initDB;
