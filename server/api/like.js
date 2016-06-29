49c1aaea6ee324ddc801c401b14086fc92ef22fc
Dongwoo Kim <idforcoding@gmail.com>
2016-06-29 16:28:59 +0900

    (feat) implement like toggle api (like.js)

--- server/api/like.js
+++ server/api/like.js
@@ -2,7 +2,7 @@ import model from "../db/models";
 import collection from "../db/collections";
 import db from "../db/config/setConfig";
 
-const report = (user, q, body, res) => {
+const like = (user, q, body, res) => {
   // const userId = user.id;
   const postId = body.postId;
 
@@ -20,12 +20,21 @@ const report = (user, q, body, res) => {
   model.Post.where("id", postId).fetch()
   .then((post) => {
     if (post) {
-      return new model.Like({userId: userId, postId: postId}).save();
+      return model.Like.where({userId: userId, postId: postId}).fetch();
     } else {
       throw "Invalid postId";
     }
   })
+  .then((like) => {
+    console.log("like is ------------, ", like);
+    if (like) {
+      return new model.Like({id: like.id}).destroy();
+    } else {
+      return new model.Like({userId: userId, postId: postId}).save();
+    }
+  })
   .then((data) => {
+    console.log("data is -------------,", data);
     res.status(200).send(data);
   })
   .catch((err) => {
@@ -35,4 +44,4 @@ const report = (user, q, body, res) => {
 
 };
 
-export default report;
+export default like;
