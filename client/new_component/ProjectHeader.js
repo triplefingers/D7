ecd38d71c8081d8bbecad9c6ede4711bff7153f4
Qjin Cho <qjin86@gmail.com>
2016-07-01 11:22:11 +0900

    (feat) Add wish toggle button to Project Detail page

--- client/new_component/ProjectHeader.js
+++ client/new_component/ProjectHeader.js
@@ -1,16 +1,64 @@
 import React, {Component} from "react";
+import axios from "axios";
 
 class ProjectHeader extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      wished: false,
+      wishCount: 0
     };
+  }
+
+  componentDidMount() {
+    const { doneWish, wishCount } = this.props.data;
+    if(doneWish){
+      this.setState({wished: true, wishCount: wishCount});
+    } else {
+      this.setState({wishCount: wishCount});
+    }
+  }
+
+  toggleWish(projectId, e) {
+    e.stopPropagation();
+    if(this.state.wished){
+      axios.post("/api/wish", {
+        projectId: projectId
+      }).then((res) => {
+        console.log('RES', res);
+        this.setState({
+          wished: false,
+          wishCount: res.data.wishCount
+        });
+      });
+    } else {
+      axios.post("/api/wish", {
+        projectId: projectId
+      }).then((res) => {
+        console.log('RES', res);
+        this.setState({
+          wished: true,
+          wishCount: res.data.wishCount
+        });
+      });
+    }
   }
 
   render() {
     console.log(this.props);
 
-    const { projectTitle, projectDescription, doneWish, userPhoto, username, wishCount } = this.props.data;
+    const { projectId, projectTitle, projectDescription, doneWish, userPhoto, username, wishCount } = this.props.data;
+
+    let wishButton;
+    if(this.state.wished){
+      wishButton = (
+        <button style={{zIndex: "10"}} onClick={this.toggleWish.bind(this, projectId)}>Wish clicked</button>
+      );
+    } else {
+      wishButton = (
+        <button style={{zIndex: "10"}} onClick={this.toggleWish.bind(this, projectId)}>Wish not clicked</button>
+      );
+    }
 
     return (
       <div>
@@ -29,8 +77,8 @@ class ProjectHeader extends Component {
         {/* Footer */}
         <div>
           <div>
-            <button>Pin</button>
-            <span>{wishCount}</span>
+            {wishButton}
+            <span>{this.state.wishCount}</span>
           </div>
           <div>
             <img src={userPhoto} width="20px" height="20px"/>
