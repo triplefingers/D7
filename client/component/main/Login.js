cccac98affb871fd9c7b21ac9880b9e16a02870c
Qjin Cho <qjin86@gmail.com>
2016-07-06 17:53:38 +0900

    (feat) Add some styling...

--- client/component/main/Login.js
+++ client/component/main/Login.js
@@ -61,24 +61,29 @@ class Login extends Component {
   render() {
     if(!this.state.isSignup){
       return (
-        <div className="text-center" style={{padding: "6rem 3rem"}}>
+        <div className="text-center login" style={{padding: "6rem 3rem"}}>
           <h1 className="form-signin-heading">D7</h1>
           <h4>Do something every single day</h4>
           <form className="form-signin" onSubmit={this.login.bind(this)}>
-            <input type="email" name="email" value={this.state.email} className="form-control" onChange={this.handleChange.bind(this, "email")} placeholder="email" style={{height: "44px"}} required/>
-            <input type="password" name="password" value={this.state.password} className="form-control" onChange={this.handleChange.bind(this, "password")} placeholder="password" style={{height: "44px"}} required/>
+            <div className="inputGroup">
+              <input type="email" name="email" value={this.state.email} className="form-control" onChange={this.handleChange.bind(this, "email")} placeholder="email" style={{height: "44px"}} required/>
+              <input type="password" name="password" value={this.state.password} className="form-control" onChange={this.handleChange.bind(this, "password")} placeholder="password" style={{height: "44px"}} required/>
+            </div>
             <button type="submit" className="btn btn-lg btn-primary btn-block">Login</button>
           </form>
           {/*<p>
             <span>or login with:</span><br />
             <button type="button">Google</button>
           </p>*/}
-          <a onClick={this.setState.bind(this, {isSignup:true})}>Sign up for D7</a>
+          <div className="gotoSignup text-center">
+            <a onClick={this.setState.bind(this, {isSignup:true})}>Sign up for D7</a>
+          </div>
+
         </div>
       );
     } else {
       return (
-        <div className="text-center" style={{padding: "6rem 3rem"}}>
+        <div className="text-center login" style={{padding: "6rem 3rem"}}>
           <h1 className="form-signin-heading">D7</h1>
           <h4>Do something every single day</h4>
           <form className="form-signin" onSubmit={this.signup.bind(this)}>
