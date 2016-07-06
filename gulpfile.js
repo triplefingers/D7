7a07120f948c63e7fc388d2b7c0ec2f27ba70308
lennyKim <heliotherapyy@gmail.com>
2016-07-06 16:45:30 +0900

    (fix) fix gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -5,30 +5,32 @@ var watch = require('gulp-watch');
 var nodemon = require('gulp-nodemon');
 var eslint = require("gulp-eslint");
 var sourcemaps = require('gulp-sourcemaps');
-
 var paths = {
-  server: ["server/**/*.js"],
-  client: ["client/**/*.js", "client/**/*.html", "!client/bundle.js", "!client/helpers/*.js", "client/**/*.css"]
+  include: {
+    server: ["server/**/*.js"],
+    client: ["client/**/*.js", "client/**/*.html", "client/**/*.css"]
+  },
+  exclude: {
+    server: ["!node_modules/**"],
+    client: ["!client/bundle.js", "!client/helpers/*.js"]
+  }
 };
-
 gulp.task("babel", () => {
-  gulp.src(paths.server)
+  return gulp.src(paths.include.server)
     .pipe(sourcemaps.init())
     .pipe(babel({
       presets: ['es2015']
     }))
     .pipe(sourcemaps.write("."))
-    .pipe(gulp.dest("build"))
+    .pipe(gulp.dest("build"));
 });
-
 gulp.task("webpack", () => {
-  gulp.src("./client/index.js")
+  return gulp.src("./client/index.js")
   .pipe(webpack(require("./webpack.config.js")))
-  .pipe(gulp.dest("./client/"))
+  .pipe(gulp.dest("./client/"));
 });
-
 gulp.task("eslint", () => {
-  gulp.src([paths.server[0], paths.client[0], paths.client[2], paths.client[3], "!node_modules/**"])
+  return gulp.src(paths.include.server.concat(paths.include.client.slice(0, 1)).concat(paths.exclude.server).concat(paths.exclude.client))
   .pipe(eslint({
     rules: {
       quotes: [1, "double"],
@@ -38,20 +40,17 @@ gulp.task("eslint", () => {
     parser: "babel-eslint"
   }))
   .pipe(eslint.format())
-  .pipe(eslint.failAfterError())
+  .pipe(eslint.failAfterError());
 });
-
 gulp.task('watch', () => {
   gulp.watch(paths.server, ["babel"]);
   gulp.watch(paths.client, ["webpack"]);
   gulp.watch(paths.server, ["eslint"]);
 });
-
 gulp.task("start", () => {
   nodemon({
     script: "build/server.js",
     watch: ["**/*"]
-  })
+  });
 });
-
 gulp.task("default", ["eslint", "babel", "webpack", "watch", "start"]);
