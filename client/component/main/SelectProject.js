1da8bfbfe2e414bb2a500b2881390f82a7530645
lennyKim <heliotherapyy@gmail.com>
2016-07-06 15:04:07 +0900

    (fix) fix bug in leaving history

--- client/component/main/SelectProject.js
+++ client/component/main/SelectProject.js
@@ -1,66 +1,3 @@
-// import React, {Component} from "react";
-// import ActionBar from "../subcomponents/ActionBar";
-// import SelectBox from "../subcomponents/SelectBox";
-
-// class SelectProject extends Component {
-//   constructor(props) {
-//     super(props);
-
-//     this.state = {};
-//   }
-
-//   componentDidMount() {
-//     this.props.fetchOngoingProjects();
-//   }
-
-//   render() {
-
-//     const onGoingProjects = this.props.data.onGoing;
-//     let projects, createNewProject;
-
-//     if (onGoingProjects) {
-//       projects = onGoingProjects.map((project) => {
-//         return (
-//           <SelectBox _save={this.props._save} project={project} />
-//           );
-//       })
-//     }
-
-//     if (!this.props.data.leaveHistoryClicked) {
-//       createNewProject = (
-//         <div>
-//           <h2>Create New Project</h2>
-//           <button onClick={() => {this.props.goto("/create"); this.props._save({creatingProjectLast: true});}}>+</button>
-//         </div>
-//       );
-//     }
-
-//     let nextUrl = undefined;
-
-//     if (this.props.data.creatingProjectFirst) {
-//       nextUrl = "/write"
-//     } else if (this.props.data.leaveHistoryClicked) {
-//       nextUrl = "/write"
-//     }
-
-//     return (
-//       <div>
-//         <div>
-//           <h2>Select Your Project</h2>
-//           <form>
-//             {projects}
-//           </form>
-//         </div>
-//         {createNewProject}
-//         <ActionBar saveDayDetail={this.props.saveDayDetail} goto={this.props.goto} project={this.props.data.selectedProject} data={this.props.data} nextUrl={nextUrl} validateAll={this.props.validateAll}/>
-//       </div>
-
-//     );
-//   }
-// }
-
-// export default SelectProject;
-
 import React, {Component} from "react";
 import ActionBar from "../subcomponents/ActionBar";
 import SelectBox from "../subcomponents/SelectBox";
@@ -94,7 +31,8 @@ class SelectProject extends Component {
       createNewProject = (
         <div>
           <h2>Create New Project</h2>
-          <button onClick={() => {this.props.goto("/create"); this.props._save({creatingProjectLast: true});}}>+</button>
+          <img id="CreateNewProject" width="100" heigh="100" src="http://www.clker.com/cliparts/A/P/L/b/V/G/blue-plus-sign-md.png"
+               onClick={() => {this.props.goto("/create"); this.props._save({creatingProjectLast: true});}} />
         </div>
       );
     }
--- client/component/subcomponents/ActionBar.js
+++ client/component/subcomponents/ActionBar.js
@@ -1,113 +1,3 @@
-// import React, {Component} from "react";
-
-// class ActionBar extends Component {
-//   constructor(props) {
-//     super(props);
-
-//     this.state = {};
-//   }
-
-//   componentDidMount() {
-
-//   }
-
-//   render() {
-
-//     /* Validation Process */
-//     let validationCallback = (item) => {
-//       if (item && item.length > 0) {
-//         return true;
-//       } else if (item) {
-//         return true;
-//       }
-//       return false;
-//     };
-
-
-
-//     if (this.props.project) {
-//       var data = this.props.project;
-//       var userProjectId = data.id;
-//       var onDay = data.onDay;
-//       var text = this.props.data.text;
-
-//       if (window.publicIds) {
-//         var publicIds = window.publicIds;
-//       } else {
-//         publicIds = [];
-//       }
-//     } else if (this.props.callback) {
-//       var data = this.props.callback.data;
-//       var userProjectId = data.selectedProject.id;
-//       var onDay = data.selectedProject.onDay;
-//       var text = data.text;
-
-//       if (window.publicIds) {
-//         var publicIds = window.publicIds;
-//       } else {
-//         publicIds = [];
-//       }
-//     }
-
-//     let rightButton;
-
-//     // If parent passes nextUrl props
-//     // onClick 할때 이동하는 url 을 props 로 받음
-//     // url이 넘어오지 않는다면 항상 SAVE -> / 로 이동
-//     if (this.props.nextUrl) {
-//       console.log("First case");
-//       let validator = () => {
-//         if (this.props.validateAll(validationCallback, this.props.data.selectedProject)) {
-//           console.log("Project Selected");
-//           this.props.goto(this.props.nextUrl)
-//         } else {
-//           alert("Check again : Pick a project before proceed");
-//         }
-//       };
-
-//       rightButton = <button onClick={validator}>Next</button>
-//     } else if ((this.props.data && (!this.props.data.leaveHistoryClicked)) ||
-//       ((this.props.callback) && this.props.callback.data.leaveHistoryInProgress)) {
-//       console.log("Second case");
-//       let validator = () => {
-//         if (this.props.validateAll(validationCallback, this.props.data.selectedProject) && this.props.callback) {
-//           console.log("Project Selected");
-//           this.props.callback.saveDayDetail(undefined, userProjectId, onDay, text, publicIds)
-//         } else if (this.props.validateAll(validationCallback, this.props.data.selectedProject)) {
-//           this.props.saveDayDetail(undefined, userProjectId, onDay, text, publicIds);
-//         } else {
-//           alert("Check again : Pick a project before proceed");
-//         }
-//       };
-
-//       rightButton = <button onClick={validator}>Save</button>
-
-//       if (this.props.callback) {
-//         rightButton = <button onClick={validator}>Save</button>
-//       }
-//     } else {
-//       console.log("Third case");
-//       rightButton = <button onClick={()=>{this.props.goto(this.props.nextUrl)}}>Next</button>
-//     }
-
-//     // if (this.props.nextUrl || this.props.data.leaveHistoryClicked) {
-//     //   console.log("ACTION BAR ----- nextUrl? ", this.props.nextUrl);
-//     //   rightButton = <button onClick={()=>{this.props.goto(this.props.nextUrl)}}>Next</button>
-//     // } else {
-//     //   rightButton = <button onClick={() => this.props.saveDayDetail(null, userProjectId, onDay, text, publicIds)}>Save</button>
-//     // }
-
-//     return (
-//       <div>
-//         <button onClick={() => this.props.goto("/")}>Back</button>
-//         {rightButton}
-//       </div>
-//     );
-//   }
-// }
-
-// export default ActionBar;
-
 import React, {Component} from "react";
 
 class ActionBar extends Component {
@@ -182,6 +72,8 @@ class ActionBar extends Component {
         if ((this.props.data && (this.props.validateAll(validationCallback, this.props.data.selectedProject))) && this.props.callback) {
           console.log("Project Selected");
           this.props.callback.saveDayDetail(undefined, userProjectId, onDay, text, publicIds)
+        } else if (!this.props.callback && this.props.validateAll(validationCallback, this.props.data.selectedProject)) {
+          this.props.saveDayDetail(undefined, userProjectId, onDay, text, publicIds);
         } else if (this.props.callback.validateAll(validationCallback, this.props.callback.data.selectedProject)) {
           this.props.callback.saveDayDetail(undefined, userProjectId, onDay, text, publicIds);
         } else {
